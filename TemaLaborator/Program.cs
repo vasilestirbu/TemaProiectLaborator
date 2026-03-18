0d5967a41e7283e05e44c5c4a16a9a1368264143
Vasi <Vasi@LAPTOP-CP8PPM3J>
2026-03-18 19:43:09 +0200

    Am adaugat si modificat Main, astfel incat sa il fac un meniu cu swicth si case options, si am facut functii precum adaugare masina, afisare si cautare masina, precum si adaugare client nou.

--- TemaLaborator/Program.cs
+++ TemaLaborator/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace InchirieriMasini
+﻿using System;
+using System.Collections.Generic;
+
+namespace InchirieriMasini
 {
     public class Masina
     {
@@ -27,5 +30,127 @@
         public Client Clientul { get; set; }
         public DateTime DataInceput { get; set; }
         public DateTime DataSfarsit { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            bool rulare = true;
+
+            List<Masina> listaMasini = new List<Masina>();
+            List<Client> listaClienti = new List<Client>();
+
+            while (rulare)
+            {
+                Console.Clear();
+                Console.WriteLine("==== Sistem Inchirieri Masini ====");
+                Console.WriteLine("1 - Adaugare masina noua.");
+                Console.WriteLine("2 - Afiseaza toate masinile.");
+                Console.WriteLine("3 - Cauta masina dupa marca.");
+                Console.WriteLine("4 - Client Nou.");
+                Console.WriteLine("5 - Iesire.");
+                Console.Write("\nSelectati o optiune:");
+
+                string optiune = Console.ReadLine();
+
+                switch (optiune)
+                {
+                    case "1":
+
+                        Masina masinaNoua = new Masina();
+
+                        Console.Write("Introduceti marca masinii:");
+                        masinaNoua.Marca = Console.ReadLine();
+
+                        Console.Write("Introduceti modelul masinii:");
+                        masinaNoua.Model = Console.ReadLine();
+
+                        Console.Write("Introduceto numarul de inmatriculare:");
+                        masinaNoua.NumarInmatriculare = Console.ReadLine();
+
+                        masinaNoua.EsteDisponibila = true;
+
+                        listaMasini.Add(masinaNoua);
+
+                        Console.WriteLine("\nMasina a fost salvata cu succes!");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\n--- Lista Masini ---");
+                        if (listaMasini.Count == 0)
+                            Console.WriteLine("Nu exista masini.");
+
+                        foreach (var masina in listaMasini)
+                        {
+                            Console.WriteLine($"- {masina.Marca} {masina.Model} [{masina.NumarInmatriculare}]");
+                        }
+
+                        break;
+
+                    case "3":
+
+                        Console.Write("\nIntroduceti marca pentru cautare: ");
+                        string cautare = Console.ReadLine();
+                        bool gasit = false;
+
+                        foreach (var masina in listaMasini)
+                        {
+                            if (masina.Marca.Equals(cautare, StringComparison.OrdinalIgnoreCase))
+                            {
+                                Console.WriteLine($"[GASIT] {masina.Marca} {masina.Model} - Nr: {masina.NumarInmatriculare}");
+                                gasit = true;
+                            }
+                        }
+
+                        if (gasit == false) Console.WriteLine("----Nu exista aceasta marca de masina in acest garaj----");
+                        break;
+
+
+
+                    case "4":
+                        Console.WriteLine("\n--- Date Client ---");
+
+                        Client clientNou = new Client();
+
+                        Console.Write("Nume client:");
+                        clientNou.Nume = Console.ReadLine();
+
+                        Console.Write("Prenume client:");
+                        clientNou.Prenume = Console.ReadLine();
+
+                        Console.Write("CNP client:");
+                        clientNou.CNP = Console.ReadLine();
+
+                        listaClienti.Add(clientNou);
+                        Console.WriteLine("\nClientul a fost adaugat cu succes!");
+
+                        break;
+
+
+                    case "5":
+                        rulare = false;
+                        Console.WriteLine("---Program terminat---");
+                        Console.WriteLine("La revedere");
+                        break;
+
+                    default:
+                        Console.WriteLine("Optiune invalida!" +
+                            "Incercati din nou.");
+                        break;
+
+                                                                
+                }
+
+                if (rulare)
+                {
+                    Console.WriteLine("\nApasati ENTER pentru meniu:");
+                    Console.ReadKey();
+                }
+
+            }
+               
+        }
     }
 }
